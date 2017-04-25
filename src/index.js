cb87efd06b6bd080b179e01fb4108d94467d2088
Seungjae Lee <saystone@gmail.com>
2017-04-25 14:17:33 +0900

    Replace square class component to functional component

--- src/index.js
+++ src/index.js
@@ -2,21 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      value: null
-    };
-  }
-
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  )
 }
 
 class Board extends React.Component {
